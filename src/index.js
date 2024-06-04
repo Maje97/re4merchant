c5554001c1a22cae1224da22126918a2cbfbea57
Alexandra Eriksson <maje@hotmail.se>
2024-06-04 15:50:11 +0200

    Fixed basename for browserrouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter baseName="/re4merchant">
+    <BrowserRouter basename="/re4merchant">
       <App />
     </BrowserRouter>
   </React.StrictMode>
